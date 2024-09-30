67794ab9c1df563f0a5e5886825fb38fe41cd4b1
iftekar2 <iftekar.hossain123@gmail.com>
2024-09-30 15:52:32 -0400

    making updates

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -4,26 +4,46 @@ import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
+        String userName = promptForUserName("What is your name: ");
+
+        int hoursWorked = promptForNumberOfHoursWorked("How many hours in a week did you work: ");
+
+        double hourlyPayRate = promptForHourlyRate("What is your hourly pay rate: ");
+
+        double grossPay = calculateGrossPay(hoursWorked, hourlyPayRate);
+
+        System.out.printf("Hi %s, your gross income is $%.2f%n", userName, grossPay);
+    }
+
+    public static String promptForUserName(String prompt) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter you name: ");
+        System.out.print(prompt);
         String name = scanner.nextLine();
+        return name;
+    }
 
-        System.out.print("Enter number of hours worked per week: ");
-        int hoursWorkedPerWeek = scanner.nextInt();
+    public static int promptForNumberOfHoursWorked(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        int hours = scanner.nextInt();
+        return hours;
+    }
 
+    public static double promptForHourlyRate(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        double hourlyRate = scanner.nextDouble();
+        return hourlyRate;
+    }
 
-        System.out.print("What is your hourly pay rate: ");
-        double hourlyPayRate = scanner.nextDouble();
-
-        double grossPay;
+    public static double calculateGrossPay(int hoursWorkedPerWeek, double hourlyPayRate){
+        double total;
         if(hoursWorkedPerWeek > 40){
             int hoursOverWorked = hoursWorkedPerWeek - 40;
-            grossPay = (40 * hourlyPayRate) + (hoursOverWorked * hourlyPayRate * 1.5);
+            total = (40 * hourlyPayRate) + (hoursOverWorked * hourlyPayRate * 1.5);
         }else{
-            grossPay = hoursWorkedPerWeek * hourlyPayRate;
+            total = hoursWorkedPerWeek * hourlyPayRate;
         }
-
-        System.out.printf("Hi %s, your gross income is $%.2f%n", name, grossPay);
+        return total;
     }
 }
