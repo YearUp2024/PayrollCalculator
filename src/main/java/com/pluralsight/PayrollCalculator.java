0584f3d40aff40f71b3e91ad57c33f3d0a1b142d
iftekar2 <iftekar.hossain123@gmail.com>
2024-09-30 15:09:54 -0400

    Making updates

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -9,13 +9,20 @@ public class PayrollCalculator {
         System.out.print("Enter you name: ");
         String name = scanner.nextLine();
 
-        System.out.print("Enter number of hours worked: ");
-        int hoursWorked = scanner.nextInt();
+        System.out.print("Enter number of hours worked per week: ");
+        int hoursWorkedPerWeek = scanner.nextInt();
+
 
         System.out.print("What is your hourly pay rate: ");
         double hourlyPayRate = scanner.nextDouble();
 
-        double grossPay = hoursWorked * hourlyPayRate;
+        double grossPay;
+        if(hoursWorkedPerWeek > 40){
+            int hoursOverWorked = hoursWorkedPerWeek - 40;
+            grossPay = (40 * hourlyPayRate) + (hoursOverWorked * hourlyPayRate * 1.5);
+        }else{
+            grossPay = hoursWorkedPerWeek * hourlyPayRate;
+        }
 
         System.out.printf("Hi %s, your gross income is $%.2f%n", name, grossPay);
     }
